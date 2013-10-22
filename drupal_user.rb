c32ff519248659fedb8633652ad6ac03100e4254
Cameron Tod <cameron.tod@gmail.com>
2013-10-22 23:22:31 +0100

    Adding basic exception handling for names that are not found.

--- drupal_user.rb
+++ drupal_user.rb
@@ -39,20 +39,23 @@ class DrupalUser
     # TODO Keep a static cache of results.
     # Use HTTP caching to avoid hitting Drupal.org too hard during development/testing.
     RestClient.enable Rack::Cache
-    if @@username_map.has_key?(name) == false
-      puts 'looking up ' + name
-      drupal_user_search_url = 'https://drupal.org/search/user_search/'
-      result_page = Nokogiri::HTML(open(drupal_user_search_url + URI.escape(name)))
-      match = result_page.css('dl.user_search-results a').first['href'].match Regexp.quote('drupal.org/user/') + '(\d+)$'
-      @@username_map[name] = match[1]
-      puts 'Found them! uid is ' + @@username_map[name]
-      puts 'Sleeping for 10 seconds...'
-      sleep(10)
-    else
-      puts 'Already have ' + name
+    begin
+      if @@username_map.has_key?(name) == false
+        puts 'looking up ' + name
+        drupal_user_search_url = 'https://drupal.org/search/user_search/'
+        result_page = Nokogiri::HTML(open(drupal_user_search_url + URI.escape(name)))
+        match = result_page.css('dl.user_search-results a').first['href'].match Regexp.quote('drupal.org/user/') + '(\d+)$'
+        @@username_map[name] = match[1]
+        puts 'Found them! uid is ' + @@username_map[name]
+        puts 'Sleeping for 5 seconds...'
+        sleep(5)
+      else
+        puts 'Already have ' + name
+      end
+        return @@username_map[name]
+    rescue NoMethodError=>e
+      puts "YAML file not loaded correctly. Error: #{e}."
     end
-
-    return @@username_map[name]
   end
 
   def get_username_map
