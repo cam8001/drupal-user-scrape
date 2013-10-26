baa4f83a571bdb135cf9857eecad998cba6c12c7
Cameron Tod <cameron.tod@gmail.com>
2013-10-26 01:24:43 +0100

    Converted scraping methods to simpler, DRYer pattern.

--- drupal_user_scrape.rb
+++ drupal_user_scrape.rb
@@ -14,48 +14,74 @@ class DrupalUserScrape
     @logger = Logger.new(STDOUT)
     @dorg_cache = DOrgCache.new()
     # Initialize all fields to empty strings to avoid nil errors later.
-    @company, @company_logo, @country, @job_title, @fullname, @website = ''
+    @company, @company_logo, @country, @job_title, @fullname, @first_name,
+    @surname, @website = ''
+
     begin
       @page ||= Nokogiri::HTML(@dorg_cache.fetch(DRUPAL_USER_PROFILE_URL + @uid))
-      self.scrape
-    rescue  TypeError=>e
+    rescue TypeError=>e
       @logger.info(self.class) {"Couldn't parse #{e}"}
     end
   end
 
-  #def company=(newCompany)
-  #  @company = newCompany
-  #end
+  # Public: scrape elements that contain text and return their contents.
   #
-  #def job_title=(newJobTitle)
-  #  @job_title = newJobTitle
-  #end
+  # css_selector - A CSS selector String for the element to be scraped.
   #
-  #def fullname=(newFullName)
-  #  @fullname = newFullName
-  #end
+  def scrape_text_element(css_selector)
+    @page.css(css_selector).each do |element|
+      return element.text
+    end
+  end
+
+  # Public: scrape a typical user field.
+  #
+  # The majority of fields follow a simple CSS pattern. This function
+  # allows access to profile fields by passing only the unique part of the CSS
+  # identifier.
+  #
+  # fieldname: String, unique component of a CSS string.
+  #
+  def scrape_field(fieldname)
+    css_selector = "dd.profile-profile_#{fieldname}.grid-6.omega"
+    self.scrape_text_element(css_selector)
+  end
 
   def company
+    # Company is a trickier parse, because it may be either a logo or an anchor.
+    if @company == ''
+      self.scrape_company
+    end
     @company
   end
 
   def company_logo
+    self.company
     @company_logo
   end
 
   def country
-    @country
+    @country ||= self.scrape_text_element('dd.profile-country.grid-6.omega a')
   end
 
   def job_title
-    @job_title
+    @job_title ||= self.scrape_field('job')
   end
 
-  def scrape
-    self.scrape_company
-    self.scrape_fullname
-    self.scrape_job_title
-    self.scrape_country
+  def fullname
+    @fullname ||= self.scrape_field('full_name')
+  end
+
+  def firstname
+    @first_name ||= self.scrape_field('first_name')
+  end
+
+  def surname
+    @surname ||= self.scape_field('last_name')
+  end
+
+  def website
+    @website ||= self.scrape_text_element('profile-homepage.grid-6.omega a')
   end
 
   def scrape_company
@@ -69,39 +95,14 @@ class DrupalUserScrape
           @company = element['alt']
       end
     end
-
-    def scrape_fullname
-      @page.css('profile-profile_full_name grid-6 omega').each do |element|
-        @fullname = element.text
-      end
-    end
-
-    def scrape_country
-      @page.css('dd.profile-country.grid-6.omega a').each do |element|
-        @country = element.text
-      end
-    end
-
-    def scrape_job_title
-      @page.css('dd.profile-profile_job.grid-6').each do |element|
-        @job_title = element.text
-      end
-    end
-
-    def scrape_website
-      @page.css('profile-homepage grid-6 omega a').each do |element|
-        @website = element.text
-      end
-    end
-
-    # Get a page object suitable for parsing.
-    # @link http://tomdoc.org/
-    #
-    # TODO: When I am less tired read the following link more carefully and
-    # review visibility. Protected is NOT the same as in Java/PHP.
-    #
-    # http://blog.zerosum.org/2007/11/22/ruby-method-visibility
-
   end
+
+  # @link http://tomdoc.org/
+  #
+  # TODO: When I am less tired read the following link more carefully and
+  # review visibility. Protected is NOT the same as in Java/PHP.
+  #
+  # http://blog.zerosum.org/2007/11/22/ruby-method-visibility
+
 
 end
